bfa9e04d0d98881ae214dd748aedbf8f153b4ca4
Bradmkjr <brad@ucanbonline.net>
2018-06-04 11:03:48 -0600

    Changed to SCSS

--- src/app/venue-detail/venue-detail.component.ts
+++ src/app/venue-detail/venue-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-venue-detail',
   templateUrl: './venue-detail.component.html',
-  styleUrls: ['./venue-detail.component.css']
+  styleUrls: ['./venue-detail.component.scss']
 })
 export class VenueDetailComponent implements OnInit {
 
--- src/app/venue/venue.component.ts
+++ src/app/venue/venue.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-venue',
   templateUrl: './venue.component.html',
-  styleUrls: ['./venue.component.css']
+  styleUrls: ['./venue.component.scss']
 })
 export class VenueComponent implements OnInit {
 
