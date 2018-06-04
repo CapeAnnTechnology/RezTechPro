6fcc9f90cb166b7fa69bdc09a7d89bf8803dfb67
Bradmkjr <brad@ucanbonline.net>
2018-06-03 19:48:47 -0600

    Sample User Data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -2,6 +2,30 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    
+    const users = [
+      { id: 1, firstName: 'Johnny', lastName: 'Walker', emailAddress: 'johnny@walker.com', createdAt: '12:34pm 06/03/2018', createdBy: '-1' },
+      { id: 2, firstName: 'Jose', lastName: 'Cuervo', emailAddress: 'jose.cuervo@gmail.com', createdAt: '12:34pm 06/03/2018', createdBy: '-1' },
+      { id: 3, firstName: 'Julia', lastName: 'Cuervo', emailAddress: 'juliacuervo86@gmail.com', createdAt: '12:34pm 06/03/2018', createdBy: '-1' },
+      { id: 4, firstName: 'Thomas', lastName: 'Franqui', emailAddress: 'ThomasAFranqui@armyspy.com', createdAt: '12:34pm 06/03/2018', createdBy: '-1' },
+      { id: 5, firstName: 'Blake', lastName: 'Stamper', emailAddress: 'blakes@teleworm.us', createdAt: '12:34pm 06/03/2018', createdBy: '-1' },
+      { id: 6, firstName: 'Lisa', lastName: 'Freeland', emailAddress: 'lisafree@gmail.com', createdAt: '12:34pm 06/03/2018', createdBy: '-1' },      
+    ];
+    return {users};
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
