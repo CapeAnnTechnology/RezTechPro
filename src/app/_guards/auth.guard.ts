1fea1ba983b1e1ee689770f6dae3f7288e544f6c
Bradmkjr <brad@ucanbonline.net>
2018-06-05 23:56:16 -0600

    removed placeholder authGuard code

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,19 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-// import { Observable } from 'rxjs';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthGuard implements CanActivate {
-//   canActivate(
-//     next: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-//     return true;
-//   }
-// }
-
-
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
