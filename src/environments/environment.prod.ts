9178cad7de9bee5b49673e2923c28de95095ac4f
Bradmkjr <brad@ucanbonline.net>
2018-06-24 13:23:47 -0600

    full path for production API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   SSO_URI: 'https://sso.reztechpro.com/',
-  API_URI: 'https://api.reztechpro.com/v2.0',
-  BASE_API: 'https://api.reztechpro.com/v2.0',
+  API_URI: 'https://api.reztechpro.com/v2.0/',
+  BASE_API: 'https://api.reztechpro.com/v2.0/',
   BASE_URI: 'https://reztechpro.com/',
   AUTH0_CLIENT_DOMAIN: 'reztechpro.auth0.com',
   AUTH0_CLIENT_ID: 'SJLqm1Itq4vSMmc8NhEl59AgV5zVAHTu',
