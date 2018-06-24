fc2b2b142c5852d64bb84db97f75248b5779f225
Bradmkjr <brad@ucanbonline.net>
2018-06-24 12:34:32 -0600

    added missing parameters

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   SSO_URI: 'https://sso.reztechpro.com/',
   API_URI: 'https://api.reztechpro.com/v2.0',
+  BASE_API: 'https://api.reztechpro.com/v2.0',
   BASE_URI: 'https://reztechpro.com/',
   AUTH0_CLIENT_DOMAIN: 'reztechpro.auth0.com',
   AUTH0_CLIENT_ID: 'SJLqm1Itq4vSMmc8NhEl59AgV5zVAHTu',
