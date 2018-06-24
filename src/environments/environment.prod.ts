ee2dc7bfcdb0c27eac724b99f9082513297ff4cf
Bradmkjr <brad@ucanbonline.net>
2018-06-24 13:43:24 -0600

    incorrect url in config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   SSO_URI: 'https://sso.reztechpro.com/',
   API_URI: 'https://reztechpro-api.herokuapp.com/v2.0/',
   BASE_API: 'https://reztechpro-api.herokuapp.com/v2.0/',
-  BASE_URI: 'https://reztechpro-www.herokuapp.com/',
+  BASE_URI: 'https://reztechpro-www.herokuapp.com',
   AUTH0_CLIENT_DOMAIN: 'reztechpro.auth0.com',
   AUTH0_CLIENT_ID: 'SJLqm1Itq4vSMmc8NhEl59AgV5zVAHTu',
   NAMESPACE: 'https://reztechpro.com/roles'
