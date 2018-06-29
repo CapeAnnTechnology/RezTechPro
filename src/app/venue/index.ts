8bb951b7563879d39bd0b7903cad03cfcd3326d7
Bradmkjr <brad@ucanbonline.net>
2018-06-29 16:43:04 -0600

    updated index for venue

--- src/app/venue/index.ts
+++ src/app/venue/index.ts
@@ -1,4 +1,3 @@
 export * from './venue.component';
-export * from './detail/detail.component';
 export * from './venue-detail/venue-detail.component';
 export * from './venue-checklists/venue-checklists.component';
